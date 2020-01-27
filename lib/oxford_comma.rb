b262e021712ddbdf6b60aeb024a65544a695004f
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-27 19:20:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  
+  case array
+   when array.length == 1
+    puts array.join
+   when array.length == 2
+    puts array.join(" and ")
+   when array.length
 
+  end
 end
