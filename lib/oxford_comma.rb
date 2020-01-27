fc4b76252d7f2e0bdf2e109a920d9e78fe96961f
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-27 19:27:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,10 @@ def oxford_comma(array)
     puts array.join
    when array.length == 2
     puts array.join(" and ")
-   when array.length
-
+   when array.length >= 3
+    last_word = array.pop
+    oxford_comma_string = array.join(", ")
+    oxford_comma_string << ", and #{last_word}"
+    puts oxford_comma_string
   end
 end
